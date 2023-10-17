56fea5646f71171cac08dca5c1d19d2cbb623b6a
Tak-key <131273734+Tak-key@users.noreply.github.com>
2023-10-17 16:45:09 +0900

    編集2
    Tronシェーダー追加、クリア処理追加。gitIgnoreのcubemapを3つIgnoreしているので、後でローカルで再DLすること

---
+++ Assets/Scripts/GoalBlock.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GoalBlock : MonoBehaviour
+{
+    [SerializeField, Header("ゲームマネージャー")]
+    GameManager gameManager;
+
+    [SerializeField, Header("プレイヤータグ")]
+    string playerTag;
+
+    [SerializeField, Header("ゴール番号")]
+    int goalNo;
+
+    BallController ballScript; //ボールのスクリプトを収納
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            ballScript = collision.gameObject.GetComponent<BallController>();
+
+            if (ballScript.isMoving)
+            {
+                return;
+            }
+            else
+            {
+                gameManager.GoalCheck(goalNo, true);
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+            gameManager.GoalCheck(goalNo, false);
+    }
+}
